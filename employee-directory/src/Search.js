86ee2b0a84ad54104fdd58978c97be940aca552d
Kathleen Tonner <kathleentonner@gmail.com>
2020-05-04 17:26:57 -0400

    fixing up search bar

--- employee-directory/src/Search.js
+++ employee-directory/src/Search.js
@@ -3,12 +3,10 @@ import './App.css';
 
 function Search() {
     return (
-        <div className="col-md-4">
 <div className="input-group mb-3">
-  <input type="text" className="form-control" id="search" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+  <input type="text" className="form-control" id="search" placeholder="Employee Name" aria-label="Recipient's username" aria-describedby="button-addon2"/>
   <div className="input-group-append">
-    <button className="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
-  </div>
+    <button className="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
   </div>
 </div>
     )
