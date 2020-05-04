183597e2cd783129ca914b3e8d055ac1b37e919c
Kathleen Tonner <kathleentonner@gmail.com>
2020-05-04 17:05:18 -0400

    basic structure added

---
+++ employee-directory/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Jumbotron() {
+  return (
+    <div className="App">
+<div class="jumbotron jumbotron-fluid">
+  <div className="container">
+    <h1 className="display-4">Employee Directory</h1>
+    <p className="lead">Search through all your employees below.</p>
+  </div>
+</div>
+    </div>
+  );
+}
+
+export default Jumbotron;
