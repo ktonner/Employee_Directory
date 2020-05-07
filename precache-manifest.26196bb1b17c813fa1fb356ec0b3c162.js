e6e7a50602efbfd7fbd9fb98eedc11e5d485e15b
Kathleen Tonner <kathleentonner@gmail.com>
2020-05-07 19:41:21 -0400

    Updates

---
+++ precache-manifest.26196bb1b17c813fa1fb356ec0b3c162.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bdf17e586686f8660a2efb5ac1bc42f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3d9c25507bcdce18ab8",
+    "url": "/static/css/main.2fb8aa4d.chunk.css"
+  },
+  {
+    "revision": "8604701688cb444834c6",
+    "url": "/static/js/2.56a40212.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56a40212.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3d9c25507bcdce18ab8",
+    "url": "/static/js/main.82859527.chunk.js"
+  },
+  {
+    "revision": "99299d3108f175eda0e9",
+    "url": "/static/js/runtime-main.3793437c.js"
+  }
+]);
